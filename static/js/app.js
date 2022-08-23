63e85ea3a39734db8f16256c6d21dada83c0cbc9
eneubauer2022 <eneubauer@csiusa.ws>
2022-08-22 23:27:43 -0400

    adding challenge files

--- static/js/app.js
+++ static/js/app.js
@@ -30,7 +30,7 @@ var filters ={};
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-let changedElement = d3.select(this).select("input");
+let changedElement = d3.select(this);
 
     // 4b. Save the value that was changed as a variable.
 let elementValue = changedElement.property("value");
